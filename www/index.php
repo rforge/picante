5944ffc1dd8a8bf215f605632c41f5c1cdb614c1
skembel <skembel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-02 02:51:14 +0000

    Update project webpage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/picante@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -32,14 +32,14 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
-<h2>picante</h2>
-<h4><strong>P</strong>hylocom <strong>i</strong>ntegration, <strong>c</strong>ommunity <strong>a</strong>nalyses, <strong>n</strong>ull-models, <strong>t</strong>raits and <strong>e</strong>volution in R</h4>
+<h1>picante</h1>
+<h2><strong>P</strong>hylocom <strong>i</strong>ntegration, <strong>c</strong>ommunity <strong>a</strong>nalyses, <strong>n</strong>ull-models, <strong>t</strong>raits and <strong>e</strong>volution in R</h2>
 
 <!-- end of project description -->
 
 <p>The picante package aims to provide a comprehensive set of tools for analyzing the phylogenetic and trait diversity of ecological communities in R, along with a set of tools for other comparative analyses and manipulations of phenotypic and phylogenetic data.</p>
 
-<p>Current features include:
+<p><strong>Current features:</strong>
 <ul>
 <li>Community phylogenetic and trait similarity measures</li>
     <ul>
@@ -47,20 +47,21 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
     <li>Mean pairwise distance and mean distance to nearest neighbour among co-occurring species (can be used with any interspecific distance measure)</li>
     <li>Correlations between species co-occurrence and phylogenetic distances</li>
     </ul>
-<li>Phylogenetic signal (Blomberg et al's K statistic and P-value based on randomization test)</li>
+<li>Phylogenetic signal (Blomberg <em>et al.</em>'s K statistic and P-value based on randomization test)</li>
 <li>Independent contrasts for traits with circular distributions</li>
 <li>Null models for community and phylogeny randomization</li>
 <li>Utility functions to read/write data in <a href="http://phylodiversity.net/phylocom/">Phylocom</a> format</li>
 <li>Tree plotting and labelling functions</li>
 </ul></p>
 
-<p>Initial public release (version 0.1) scheduled for April 2008.</p>
+<p>Version 0.1-2 (initial public release) was submitted to CRAN on May 30 2008. Downloading from CRAN will soon be the recommended way to install picante, but in the meantime you can grab the latest build of the package <a href="http://r-forge.r-project.org/R/?group_id=134">here</a>, or by typing <strong><code>install.packages("picante",repos="http://R-Forge.R-project.org")</code></strong> from within R.</p>
 
-<p>Maintainer: Steve Kembel</p>
+<p><strong>Maintainer:</strong> Steve Kembel</p>
 
-<p>Contributors: David Ackerly, Simon Blomberg, Peter Cowan,  Steve Kembel, Cam Webb<p>
+<p><strong>Contributors:</strong> David Ackerly, Simon Blomberg, Peter Cowan,
+    Matthew Helmus, Steve Kembel, Cam Webb<p>
 
-<p>The <strong>project summary page</strong> is <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>.</p>
+<p>The <strong>project summary page</strong> with bug reporting, feature requests, discussions, mailing lists, and access to the source code is located <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>.</p>
 
 </body>
 </html>
