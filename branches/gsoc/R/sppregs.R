5d6c19f14cbfd2e759c02eff324dde89ac6eddea
mrhelmus <mrhelmus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-21 05:30:58 +0000

    Added the plot function to the sppregs.R file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/picante@115 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/gsoc/R/sppregs.R
+++ branches/gsoc/R/sppregs.R
@@ -107,3 +107,13 @@ sppregs<-function(samp,env,tree=NULL,fam="binomial"){
  
   }
 }
+
+plot.sppregs<-function(sppreg,rows=c(1,3),cex.mag=1,x.label="phylogenetic correlations",y.label=c("occurrence correlations w/ env","occurrence correlations wo/ env","change in correlations")){
+  par(mfrow=rows,las=1,cex=cex.mag)
+  plot(sppreg$cors.phylo,sppreg$cors.pa,xlab=x.label,ylab=y.label[1],main=paste("cor =",round(cor(sppreg$cors.phylo,sppreg$cors.pa,use="pairwise.complete.obs"),4)))
+  abline(0,0,lty=2)
+  plot(sppreg$cors.phylo,sppreg$cors.resid,xlab=x.label,ylab=y.label[2],main=paste("cor =",round(cor(sppreg$cors.phylo,sppreg$cors.resid,use="pairwise.complete.obs"),4)))
+  abline(0,0,lty=2)
+  plot(sppreg$cors.phylo,sppreg$cors.resid-sppreg$cors.pa,xlab=x.label,ylab=y.label[3],main=paste("cor =",round(cor(sppreg$cors.phylo,sppreg$cors.resid-sppreg$cors.pa,use="pairwise.complete.obs"),4)))
+  abline(0,0,lty=2)
+}
