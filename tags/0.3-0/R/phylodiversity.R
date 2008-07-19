676da8aea714a3b5fedf7d427a152984d2715c80
skembel <skembel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-19 01:14:35 +0000

    0.3-0 release prepared and tagged.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/picante@142 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tags/0.3-0/R/phylodiversity.R
@@ -0,0 +1,482 @@
+`comm.phylo.cor` <-
+function(samp,phylo,metric=c("cij","checkerboard","jaccard","roij"),
+		null.model=c("sample.taxa.labels","pool.taxa.labels",
+					"frequency","richness","weighted.sample.pool"),
+					runs=99)
+{
+	metric <- match.arg(metric)
+	null.model <- match.arg(null.model)
+	results <- list("obs.corr"=NA,"obs.corr.p"=NA,"obs.rank"=NA,"runs"=runs,
+			"obs.rand.p"=NA,"random.corrs"=vector(length=runs))
+	phylo.dist <- as.dist(cophenetic(prune.sample(samp,phylo)))
+	pool.phylo.dist <- as.dist(cophenetic(phylo))
+	taxa.names <- rownames(as.matrix(phylo.dist))
+	samp.dist <- as.dist(as.matrix(species.dist(samp,metric))[taxa.names,taxa.names])
+	results$obs.corr <- cor(phylo.dist,samp.dist,use="pairwise")
+	results$obs.corr.p <- cor.test(phylo.dist,samp.dist)$p.value
+	if (null.model=="sample.taxa.labels") for (run in 1:runs)
+	{
+		phylo.dist <- as.dist(taxaShuffle(as.matrix(phylo.dist))[taxa.names,taxa.names])
+		results$random.corrs[run] <- cor(phylo.dist,samp.dist,use="pairwise")
+	}
+	else if (null.model=="pool.taxa.labels") for (run in 1:runs)
+	{
+		phylo.dist <- as.dist(taxaShuffle(as.matrix(pool.phylo.dist))[taxa.names,taxa.names])
+		results$random.corrs[run] <- cor(phylo.dist,samp.dist,use="pairwise")
+	}
+	else if (null.model=="weighted.sample.pool") for (run in 1:runs)
+	{
+		samp.dist <- species.dist(randomizeSample(samp,null.model="both"),metric)
+		phylo.dist <- as.dist(as.matrix(pool.phylo.dist)[rownames(as.matrix(samp.dist)),
+							colnames(as.matrix(samp.dist))])	
+		results$random.corrs[run] <- cor(phylo.dist,samp.dist,use="pairwise")
+	}
+	else for (run in 1:runs)
+	{
+		samp.dist <- species.dist(randomizeSample(samp,null.model),metric)
+		results$random.corrs[run] <- cor(phylo.dist,samp.dist,use="pairwise")
+	}
+	results$obs.rank <- rank(as.vector(c(results$obs.corr,results$random.corrs)))[1]
+	results$obs.rand.p <- results$obs.rank/(runs+1)
+	results
+}
+
+
+mpd <- function(samp, dis) 
+{
+    N <- dim(samp)[1]
+    mpd <- numeric(N)
+    for (i in 1:N) {
+        sppInSample <- names(samp[i, samp[i, ] > 0])
+        if (length(sppInSample) > 1) {
+            sample.dis <- dis[sppInSample, sppInSample]
+            mpd[i] <- mean(sample.dis[lower.tri(sample.dis)])
+        }
+        else{
+            mpd[i] <- 0
+        }
+    }
+    mpd
+}
+
+
+`mnnd` <-
+function(samp,dis) {
+	N <- dim(samp)[1]
+	mnnd <- numeric(N)
+	for (i in 1:N) {
+		sppInSample <- names(samp[i,samp[i,]>0])
+		if (length(sppInSample) > 1) {
+            sample.dis <- dis[sppInSample,sppInSample]
+            diag(sample.dis) <- NA
+		    mnnd[i] <- mean(apply(sample.dis,2,min,na.rm=TRUE))
+		}
+		else {
+		    mnnd[i] <- 0
+		}
+	}
+	mnnd
+}
+
+`ses.mpd` <-
+function (samp, dis, null.model = c("taxa.labels", "sample.pool", 
+    "phylogeny.pool", "weighted.sample.pool"), runs = 99) 
+{
+    dis <- as.matrix(dis)
+    mpd.obs <- mpd(samp, dis)
+    null.model <- match.arg(null.model)
+    mpd.rand <- switch(null.model,
+    	taxa.labels = t(replicate(runs, mpd(samp, taxaShuffle(dis)))),
+    	sample.pool = t(replicate(runs, mpd(randomizeSample(samp,null.model="richness"), dis))),
+    	phylogeny.pool = t(replicate(runs, mpd(randomizeSample(samp,null.model="richness"),
+    		taxaShuffle(dis)))),
+    	weighted.sample.pool = t(replicate(runs, mpd(randomizeSample(samp,
+    		null.model = "both"), dis))))
+    mpd.obs.rank <- apply(X = rbind(mpd.obs, mpd.rand), MARGIN = 2, 
+        FUN = rank)[1, ]
+    mpd.rand.mean <- apply(X = mpd.rand, MARGIN = 2, FUN = mean, na.rm=TRUE)
+    mpd.rand.sd <- apply(X = mpd.rand, MARGIN = 2, FUN = sd, na.rm=TRUE)
+    mpd.obs.z <- (mpd.obs - mpd.rand.mean)/mpd.rand.sd
+    data.frame(ntaxa=specnumber(samp),mpd.obs, mpd.rand.mean, mpd.rand.sd, mpd.obs.rank, 
+        mpd.obs.z, mpd.obs.p=mpd.obs.rank/(runs+1),runs=runs, row.names = row.names(samp))
+}
+
+`ses.mnnd` <-
+function (samp, dis, null.model = c("taxa.labels", "sample.pool", 
+    "phylogeny.pool", "weighted.sample.pool"), runs = 99) 
+{
+    dis <- as.matrix(dis)
+    mnnd.obs <- mnnd(samp, dis)
+    null.model <- match.arg(null.model)
+    mnnd.rand <- switch(null.model,
+    	taxa.labels = t(replicate(runs, mnnd(samp, taxaShuffle(dis)))),
+    	sample.pool = t(replicate(runs, mnnd(randomizeSample(samp,null.model="richness"), dis))),
+    	phylogeny.pool = t(replicate(runs, mnnd(randomizeSample(samp,null.model="richness"),
+    		taxaShuffle(dis)))),
+    	weighted.sample.pool = t(replicate(runs, mnnd(randomizeSample(samp,
+    		null.model = "both"), dis))))
+    mnnd.obs.rank <- apply(X = rbind(mnnd.obs, mnnd.rand), MARGIN = 2, 
+        FUN = rank)[1, ]
+    mnnd.rand.mean <- apply(X = mnnd.rand, MARGIN = 2, FUN = mean, na.rm=TRUE)
+    mnnd.rand.sd <- apply(X = mnnd.rand, MARGIN = 2, FUN = sd, na.rm=TRUE)
+    mnnd.obs.z <- (mnnd.obs - mnnd.rand.mean)/mnnd.rand.sd
+    data.frame(ntaxa=specnumber(samp),mnnd.obs, mnnd.rand.mean, mnnd.rand.sd, mnnd.obs.rank, 
+        mnnd.obs.z, mnnd.obs.p=mnnd.obs.rank/(runs+1),runs=runs, row.names = row.names(samp))
+}
+
+psv<-function(samp,tree,compute.var=TRUE){
+  # Make samp matrix a pa matrix
+  samp[samp>0]<-1
+  
+  flag=0
+  if (is.null(dim(samp))) #if the samp matrix only has one site
+  {
+    samp<-rbind(samp,samp)
+    flag=2
+  }
+
+  if(is(tree)[1]=="phylo")
+  {
+    if(is.null(tree$edge.length)){tree<-compute.brlen(tree, 1)}  #If phylo has no given branch lengths
+    tree<-prune.sample(samp,tree)
+    # Make sure that the species line up
+    samp<-samp[,tree$tip.label]
+    # Make a correlation matrix of the species pool phylogeny
+    Cmatrix<-vcv.phylo(tree,cor=TRUE)
+  } else {
+    Cmatrix<-tree
+    species<-colnames(samp)
+    preval<-colSums(samp)/sum(samp)
+    species<-species[preval>0]
+    Cmatrix<-Cmatrix[species,species]
+    samp<-samp[,colnames(Cmatrix)]
+  }
+  
+    # numbers of locations and species
+    SR<-rowSums(samp)
+    nlocations<-dim(samp)[1]
+    nspecies<-dim(samp)[2]
+  
+    ##################################
+    # calculate observed PSVs
+    #
+    PSVs<-NULL
+  
+    for(i in 1:nlocations)
+    {
+      index<-seq(1:nrow(Cmatrix))[samp[i,]==1]	#species present
+      n<-length(index)			#number of species present
+      if(n>1)
+      {
+        C<-Cmatrix[index,index]	#C for individual locations
+        PSV<-(n*sum(diag(as.matrix(C)))-sum(C))/(n*(n-1))
+      } else {
+        PSV<-NA
+      }
+        PSVs<-c(PSVs,PSV)
+    }
+    PSVout<-cbind(PSVs,SR)
+  
+  if(flag==2)
+  {
+    PSVout<-PSVout[-2,]
+    return(PSVout)
+  } else {
+    if(compute.var==FALSE) 
+    {
+      return(data.frame(PSVout))
+    } else {
+    
+      PSVvar<-NULL
+      X<-Cmatrix-(sum(sum(Cmatrix-diag(nspecies))))/(nspecies*(nspecies-1));
+      X<-X-diag(diag(X))
+  
+      SS1<-sum(X*X)/2
+  
+      SS2<-0;
+      for(i in 1:(nspecies-1)){
+        for(j in (i+1):nspecies){
+          SS2<-SS2+X[i,j]*(sum(X[i,])-X[i,j])
+        }
+      }
+      SS3<--SS1-SS2
+  
+      S1<-SS1*2/(nspecies*(nspecies-1))
+      S2<-SS2*2/(nspecies*(nspecies-1)*(nspecies-2))
+  
+      if(nspecies==3){
+        S3<-0
+      }else{
+      S3<-SS3*2/(nspecies*(nspecies-1)*(nspecies-2)*(nspecies-3))
+      }
+  
+      for(n in 2:nspecies){
+        approxi<-2/(n*(n-1))*(S1 + (n-2)*S2 + (n-2)*(n-3)*S3)
+        PSVvar<-rbind(PSVvar, c(n, approxi))
+      }
+  
+      vars<-rep(0,nlocations)
+      PSVout<-cbind(PSVout,vars)
+  
+      for (g in 1:nlocations)
+      {
+        if(PSVout[g,2]>1)
+        {
+          PSVout[g,3]<-PSVvar[PSVout[g,2]-1,2]
+        } else {
+          PSVout[g,3]<-NA
+        }
+      }
+      return(data.frame(PSVout))
+    }
+  }
+}
+
+psr <- function(samp,tree,compute.var=TRUE){
+  PSVout<-psv(samp,tree,compute.var)
+  if(is.null(dim(PSVout))==TRUE) 
+  {
+    PSRout<-data.frame(cbind(PSVout[1]*PSVout[2],PSVout[2]))
+    names(PSRout)<-c("PSR","SR")
+    rownames(PSRout)<-""
+    return(PSRout)
+  } else {
+    PSRout<-cbind(PSVout[,1]*PSVout[,2],PSVout[,2])
+    if(compute.var!=TRUE) {
+      colnames(PSRout)<-c("PSR","SR")
+      rownames(PSRout)<-rownames(PSVout)
+      return(data.frame(PSRout))
+    } else {
+      PSRout<-cbind(PSRout,PSVout[,3]*(PSVout[,2])^2)       
+      colnames(PSRout)<-c("PSR","SR","vars")
+      rownames(PSRout)<-rownames(PSVout)
+      return(data.frame(PSRout))
+    }
+  }
+}
+
+pse<-function(samp,tree){
+  flag=0
+  if (is.null(dim(samp))) #if the samp matrix only has one site
+  {
+    samp<-rbind(samp,samp)
+    flag=2
+  }
+
+  samp<-as.matrix(samp)
+  
+  if(is(tree)[1]=="phylo")
+  {
+    if(is.null(tree$edge.length)){tree<-compute.brlen(tree, 1)}  #If phylo has no given branch lengths
+    tree<-prune.sample(samp,tree) 
+    # Make sure that the species line up
+    samp<-samp[,tree$tip.label]
+    # Make a correlation matrix of the species pool phylogeny
+    Cmatrix<-vcv.phylo(tree,cor=TRUE)
+  } else {
+    Cmatrix<-tree
+    species<-colnames(samp)
+    preval<-colSums(samp)/sum(samp)
+    species<-species[preval>0]
+    Cmatrix<-Cmatrix[species,species]
+    samp<-samp[,colnames(Cmatrix)]
+  }
+  
+  # numbers of locations and species
+  SR<-apply(samp>0,1,sum)
+  nlocations<-dim(samp)[1]
+  nspecies<-dim(samp)[2]
+
+  #################################
+  # calculate observed phylogenetic species evenness
+  PSEs<-NULL
+  for(i in 1:nlocations){
+    index<-seq(1,ncol(Cmatrix))[samp[i,]>0]	  # species present
+    n<-length(index)                          #location species richness
+    if(n>1)
+    {    
+    C<-Cmatrix[index,index]		                #C for individual locations
+    N<-sum(samp[i,])                          #location total abundance
+    M<-samp[i,samp[i,]>0]                  #species abundance column
+    mbar<-mean(M)                             #mean species abundance
+    PSE<-(N*t(diag(as.matrix(C)))%*%M-t(M)%*%as.matrix(C)%*%M)/(N^2-N*mbar)   #phylogenetic species evenness
+    } else {PSE<-NA}
+    PSEs<-c(PSEs,PSE)
+  }
+  PSEout=cbind(PSEs,SR)
+  if (flag==2)
+  {
+    PSEout<-PSEout[-2,]
+    return(PSEout)  
+  } else {
+    return(data.frame(PSEout))
+  }
+}
+
+
+
+psc<-function(samp,tree){
+  # Make samp matrix a pa matrix
+  samp[samp>0]<-1
+  flag=0
+  if (is.null(dim(samp))) #if the samp matrix only has one site
+  {
+    samp<-rbind(samp,samp)
+    flag=2
+  }
+
+  if(is(tree)[1]=="phylo")
+  {
+    if(is.null(tree$edge.length)){tree<-compute.brlen(tree, 1)}  #If phylo has no given branch lengths
+    tree<-prune.sample(samp,tree)
+    # Make sure that the species line up
+    samp<-samp[,tree$tip.label]
+    # Make a correlation matrix of the species pool phylogeny
+    Cmatrix<-vcv.phylo(tree,cor=TRUE)
+  } else {
+    Cmatrix<-tree
+    species<-colnames(samp)
+    preval<-colSums(samp)/sum(samp)
+    species<-species[preval>0]
+    Cmatrix<-Cmatrix[species,species]
+    samp<-samp[,colnames(Cmatrix)]
+  }
+
+  # numbers of locations and species
+  SR<-rowSums(samp)
+  nlocations<-dim(samp)[1]
+  nspecies<-dim(samp)[2]
+
+  ##################################
+  # calculate observed PSCs
+  #
+  PSCs<-NULL
+
+  for(i in 1:nlocations)
+  {
+    index<-seq(1:nrow(Cmatrix))[samp[i,]==1]	#species present
+    n<-length(index)			#number of species present
+    if(n>1)
+    {    
+      C<-Cmatrix[index,index]	#C for individual locations
+      diag(C)<--1
+      PSC<-sum(apply(C,1,max))/n
+    } else {PSC<-NA}
+      PSCs<-c(PSCs,PSC)
+  }
+    PSCout<-cbind(PSCs,SR)
+ if (flag==2)
+  {
+    PSCout<-PSCout[-2,]
+    return(PSCout)  
+  } else {
+    return(data.frame(PSCout))
+  }
+}
+
+psv.spp<-function(samp,tree){
+  # Make samp matrix a pa matrix
+  samp[samp>0]<-1
+  if (is.null(dim(samp))) #if the samp matrix only has one site
+  {
+    samp<-rbind(samp,samp)
+  }  
+  if(is(tree)[1]=="phylo")
+  {
+    if(is.null(tree$edge.length)){tree<-compute.brlen(tree, 1)}  #If phylo has no given branch lengths
+    tree<-prune.sample(samp,tree)
+    # Make sure that the species line up
+    samp<-samp[,tree$tip.label]
+    # Make a correlation matrix of the species pool phylogeny
+    Cmatrix<-vcv.phylo(tree,cor=TRUE)
+  } else {
+    Cmatrix<-tree
+    species<-colnames(samp)
+    preval<-colSums(samp)/sum(samp)
+    species<-species[preval>0]
+    Cmatrix<-Cmatrix[species,species]
+    samp<-samp[,colnames(Cmatrix)]
+  }
+  # reduce given Cmatrix to the species observed in samp
+  samp<-samp[rowSums(samp)>1,] # prune out locations with <2 species
+
+  #cull the species that are not found in the samp set after all communities with 1 species are removed
+  preval<-colSums(samp)/sum(samp)
+  indexcov<-preval>0
+  Cmatrix<-Cmatrix[indexcov,indexcov]
+  samp<-samp[,indexcov]
+
+  obs.PSV<-mean(psv(samp,Cmatrix,compute.var=FALSE)[,1],na.rm=TRUE)
+
+  # numbers of locations and species
+  nlocations<-dim(samp)[1]
+  nspecies<-dim(samp)[2]
+
+  spp.PSVs<-NULL
+  for (j in 1:nspecies)
+  {
+    spp.samp<-samp[,-j]
+    spp.Cmatrix<-Cmatrix[-j,-j]
+    spp.PSV<-mean(psv(spp.samp,spp.Cmatrix,compute.var=FALSE)[,1],na.rm=TRUE)
+    spp.PSVs<-c(spp.PSVs,spp.PSV)
+  }
+  spp.PSVout<-(spp.PSVs-obs.PSV)/sum(abs(spp.PSVs-obs.PSV))
+  names(spp.PSVout)<-colnames(samp)
+  return(spp.PSVout)
+}
+
+psd<-function(samp,tree,compute.var=TRUE){
+  if (is.null(dim(samp))) #if the samp matrix only has one site
+  {
+    PSDout<-data.frame(c(psv(samp,tree,compute.var)[1],psc(samp,tree)[1],psr(samp,tree,compute.var)[1],pse(samp,tree)))
+    names(PSDout)<-c("PSV","PSC","PSR","PSE","SR")
+    return(PSDout)
+  } else {
+    if (compute.var==TRUE)
+    {
+      PSDout<-cbind(psv(samp,tree,compute.var)[,c(1,3)],psc(samp,tree)[,1],psr(samp,tree,compute.var)[,c(1,3)],pse(samp,tree))
+      colnames(PSDout)<-c("PSV","var.PSV","PSC","PSR","var.PSR","PSE","SR")
+    } else {
+      PSDout<-cbind(psv(samp,tree,compute.var)[,1],psc(samp,tree)[,1],psr(samp,tree,compute.var)[,1],pse(samp,tree))
+      colnames(PSDout)<-c("PSV","PSC","PSR","PSE","SR")
+    }
+    return(data.frame(PSDout))
+  }
+}
+
+pd<-function(samp,tree){
+  
+  if(is.null(tree$edge.length)){tree<-compute.brlen(tree, 1)}  #If phylo has no given branch lengths
+  # Make sure that the species line up
+  tree<-prune.sample(samp,tree)
+  samp<-samp[,tree$tip.label]
+  species<-colnames(samp)
+  
+  # numbers of locations and species
+  SR<-rowSums(samp)
+  nlocations=dim(samp)[1]
+  nspecies=dim(samp)[2]
+
+  ##################################
+  # calculate observed PDs
+  #
+  PDs=NULL
+
+  for(i in 1:nlocations)
+  {  
+    index<-species[samp[i,]==0]	#species not in sample
+    if(length(index)>=(nspecies-1))
+    {
+      PD<-NA
+    } else {
+      sub.tree<-drop.tip(tree,index)
+      PD<-sum(sub.tree$edge.length)
+    }
+      PDs<-c(PDs,PD)
+  }
+  
+  PDout<-data.frame(cbind(PDs,SR))
+  rownames(PDout)<-rownames(samp) 
+  return(PDout)
+}
+
--- www/index.php
+++ www/index.php
@@ -40,6 +40,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <h2>About picante</h2>
 <p>The picante package aims to provide a comprehensive set of tools for analyzing the phylogenetic and trait diversity of ecological communities in R, along with a set of tools for other comparative analyses and manipulations of phenotypic and phylogenetic data.</p>
 <p><strong>Package maintainer:</strong> Steve Kembel</p>
+<p><strong>Developers:</strong> Peter Cowan, Matthew Helmus, Steve Kembel</p>
 <p><strong>Contributors:</strong> David Ackerly, Simon Blomberg, Peter Cowan,
     Matthew Helmus, Steve Kembel, Cam Webb<p>
 <p>Development of picante has been supported by <a href="http://nserc.ca">NSERC</a>, <a href="http://www.nescent.org/index.php">NESCent</a>, and the <a href="http://code.google.com/soc/2008/">Google Summer of Code</a>.
@@ -51,25 +52,29 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
     <li>Webb's NRI/NTI and related measures of standardized effect size of community structure</li>
     <li>Mean pairwise distance and mean distance to nearest neighbour among co-occurring species (can be used with any interspecific distance measure)</li>
     <li>Correlations between species co-occurrence and phylogenetic distances</li>
+    <li>Phylogenetic diversity measures of Helmus et al. (2007).</li>
+    <li>Phylogenetic community-environment regressions of Helmus et al. (2007).
     </ul>
 <li>Phylogenetic signal (Blomberg <em>et al.</em>'s K statistic and P-value based on randomization test)</li>
 <li>Independent contrasts for traits with circular distributions</li>
 <li>Null models for community and phylogeny randomization</li>
+<li>Ives and Godfray's (2006) phylogenetic bipartite linear model.
 <li>Utility functions to read/write data in <a href="http://phylodiversity.net/phylocom/">Phylocom</a> format</li>
 <li>Tree plotting and labelling functions</li>
 </ul>
 
 <h2>Obtaining picante</h2>
 <ul>
-<li>Version 0.2-0 (stable) is available on <a href="http://cran.r-project.org/">CRAN</a>. Simply type <strong><code>install.packages("picante")</code></strong> from within R.</li>
+<li>Version 0.3-0 (stable) is available on <a href="http://cran.r-project.org/">CRAN</a>. Simply type <strong><code>install.packages("picante")</code></strong> from within R.</li>
 <li>You can grab the latest nightly build <a href="http://r-forge.r-project.org/R/?group_id=134">here</a>, or by typing <strong><code>install.packages("picante",repos="http://R-Forge.R-project.org")</code></strong> from within R.</li>
 </ul>
 
 <h2>Obtaining help and more information about picante</h2>
-<p>The <strong>project summary page</strong> with bug reporting, feature requests, discussions, mailing lists, and access to the source code is located <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>.</p>
+<p>Visit the <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>project summary page</strong></a> for bug reporting, feature requests, discussions, mailing lists, and access to the source code. If you have problems or questions about the code, please read the function documentation ( <code><strong>help(picante)</strong></code> ), and get in touch with us using one of the above methods.</p>
 
 <h2>Release history</h2>
 <p><ul>
+<li>Version 0.3-0: Bugfixes. Added phylogenetic bipartite linear models, phylogenetic community-environment regressions.</li>
 <li>Version 0.2-0: Added new community phylogenetic structure metrics (PSR/PSV/PSC methods of Helmus et al.) and phylogenetic species richness accumulation curves</li>
 <li>Version 0.1-2: Initial public release</li>
 </ul>
