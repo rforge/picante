480addb943d33eef7e391301818958b7e375cd3c
skembel <skembel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-23 04:23:19 +0000

    Format picante.c to fix indent ugliness and add include R.h to eliminate implicit declaration warnings
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/picante@149 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/gsoc/src/picante.c
+++ branches/gsoc/src/picante.c
@@ -1,75 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <R.h>
 #include <Rmath.h>
 
 int intrand(int n) {
-  double u;
-  u = unif_rand();
-  return((int)(u*n));
+    double u;
+    u = unif_rand();
+    return((int)(u*n));
 }
 
 /* inefficient but probably not important;
  * allocate and copy in vector to matrix */
 double **vectomat(double *v,int row,int column) {
-  int i,j;
-  double **m;
-
-  m = (double **)R_alloc(row,sizeof(double *));
-  for (i=0; i<row; i++) {
-    m[i] = (double *)R_alloc(column,sizeof(double));
-    for (j=0; j<column; j++) {
-      m[i][j] = v[row*j+i];  /* R uses column-first ordering */
+    int i,j;
+    double **m;
+    
+    m = (double **)R_alloc(row,sizeof(double *));
+    for (i=0; i<row; i++) {
+        m[i] = (double *)R_alloc(column,sizeof(double));
+        for (j=0; j<column; j++) {
+            m[i][j] = v[row*j+i];  /* R uses column-first ordering */
+        }
     }
-  }
-  return(m);
+    return(m);
 }
 
 /* copy matrix back into vector */
 void mattovec(double *v,double **m,int row,int column) {
-  int i,j,k;
-  k=0;
-  for (j=0; j<column; j++) {
-    for (i=0; i<row; i++) {
-      v[k++] = m[i][j];
-    } 
-  }
+    int i,j,k;
+    k=0;
+    for (j=0; j<column; j++) {
+        for (i=0; i<row; i++) {
+            v[k++] = m[i][j];
+        } 
+    }
 }
 
 
 void trialswap(double *v, int *pintervals, int * prow, int * pcolumn) {
-  long int trial;
-  int i,j,k,l;
-  int row, column;
-  int intervals;
+    long int trial;
+    int i,j,k,l;
+    int row, column;
+    int intervals;
 	double tmp;
-  double **m;
-
-  row = *prow;
-  column = *pcolumn;
-  intervals = *pintervals;
-
-  m = vectomat(v,row,column);
-
-  GetRNGstate();
-  for(trial=0;trial<intervals;trial++)
+    double **m;
+    
+    row = *prow;
+    column = *pcolumn;
+    intervals = *pintervals;
+    
+    m = vectomat(v,row,column);
+    
+    GetRNGstate();
+    for(trial=0;trial<intervals;trial++)
     {
-      i=intrand(row);
-      while((j=intrand(row))==i);
-      k=intrand(column);
-      while((l=intrand(column))==k);
-      if((m[i][k]>0.0 && m[j][l]>0.0 && m[i][l]+m[j][k]==0.0)||(m[i][k]+m[j][l]==0.0 && m[i][l]>0.0 && m[j][k]>0.0))
+        i=intrand(row);
+        while((j=intrand(row))==i);
+        k=intrand(column);
+        while((l=intrand(column))==k);
+        if((m[i][k]>0.0 && m[j][l]>0.0 && m[i][l]+m[j][k]==0.0)||(m[i][k]+m[j][l]==0.0 && m[i][l]>0.0 && m[j][k]>0.0))
 		{
 			//currently swaps abundances within columns (=species)
 			//should have a switch to swap abundances within rows, columns, or random
-		  tmp = m[i][k];
-		  m[i][k] = m[j][k];
-		  m[j][k] = tmp;
-		  tmp = m[i][l];
-		  m[i][l] = m[j][l];
-		  m[j][l] = tmp;
+			tmp = m[i][k];
+			m[i][k] = m[j][k];
+			m[j][k] = tmp;
+			tmp = m[i][l];
+			m[i][l] = m[j][l];
+			m[j][l] = tmp;
 		}
     }
-  mattovec(v,m,row,column);
-  PutRNGstate();
+    mattovec(v,m,row,column);
+    PutRNGstate();
 }
