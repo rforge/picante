3995d4574caa63561733854c93a5bca974fefa88
skembel <skembel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-24 23:06:10 +0000

    Implement simple richness-frequency null code in C, cleaning up randomizeSample R code to allow comparison of different nulls.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/picante@154 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/gsoc/src/picante.c
+++ branches/gsoc/src/picante.c
@@ -153,3 +153,102 @@ void richnessindex(double *v, int *prow, int *pcolumn)
 
     PutRNGstate();
 }
+
+void richness(double *v, int *prow, int *pcolumn)
+{
+
+    int i,j,k,l;
+    int row, column;
+	double tmp;
+    double **m;
+
+    row = *prow;
+    column = *pcolumn;
+
+    m = vectomat(v,row,column);
+
+    GetRNGstate();
+    for(i=0;i<row;i++)
+    {
+        for (j=0;j<column;j++)
+        {
+            while((k=intrand(column))==j);//choose another column (species) at random
+            tmp = m[i][j];
+            m[i][j] = m[i][k];
+            m[i][k] = tmp;
+		}
+    }
+    mattovec(v,m,row,column);
+    PutRNGstate();
+}
+
+void frequency(double *v, int *prow, int *pcolumn)
+{
+
+    int i,j,k,l;
+    int row, column;
+	double tmp;
+    double **m;
+
+    row = *prow;
+    column = *pcolumn;
+
+    m = vectomat(v,row,column);
+
+    GetRNGstate();
+    for(i=0;i<column;i++)
+    {
+        for (j=0;j<row;j++)
+        {
+            while((k=intrand(row))==j);//choose another row (sample) at random
+            tmp = m[j][i];
+            m[j][i] = m[k][i];
+            m[k][i] = tmp;
+		}
+    }
+    mattovec(v,m,row,column);
+    PutRNGstate();
+}
+
+void independentswap(double *v, int *pintervals, int *prow, int *pcolumn)
+{
+    long int swap;
+    int swapped;
+    int i,j,k,l;
+    int row, column;
+    int intervals;
+	double tmp;
+    double **m;
+
+    row = *prow;
+    column = *pcolumn;
+    intervals = *pintervals;
+
+    m = vectomat(v,row,column);
+
+    GetRNGstate();
+    for(swap=0;swap<intervals;swap++)
+    {
+        swapped = 0;
+        while(swapped==0) {
+            i=intrand(row);//Choose a random row
+            while((j=intrand(row))==i);//make sure that you do not randomly choose the same row as before
+            k=intrand(column);//Choose a random column
+            while((l=intrand(column))==k);//make sure that you do not randomly choose the same column as before
+            if((m[i][k]>0.0 && m[j][l]>0.0 && m[i][l]+m[j][k]==0.0)||(m[i][k]+m[j][l]==0.0 && m[i][l]>0.0 && m[j][k]>0.0))
+            {
+                //currently swaps abundances within columns (=species)
+                //should have a switch to swap abundances within rows, columns, or random
+                tmp = m[i][k];
+                m[i][k] = m[j][k];
+                m[j][k] = tmp;
+                tmp = m[i][l];
+                m[i][l] = m[j][l];
+                m[j][l] = tmp;
+                swapped = 1;
+            }
+        }
+    }
+    mattovec(v,m,row,column);
+    PutRNGstate();
+}
