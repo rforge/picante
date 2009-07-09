976e27f979372b25d4e8030e47855e68dd5a9276
skembel <skembel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-09 21:04:32 +0000

    Add traffic monitor to website
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/picante@191 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -122,6 +122,17 @@ S.W. Kembel, D.D. Ackerly, S.P. Blomberg, P.D. Cowan, M.R. Helmus, H. Morlon and
 </tr>
 </table>
 </div>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-9707378-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
 </body>
 </html>
 
