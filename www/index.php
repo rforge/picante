8b3bf63dedc657798b53e3996a93e9cc42430dc4
skembel <skembel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-03-18 17:15:30 +0000

    Update webpage to point to github
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/picante@248 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -32,92 +32,13 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <h2>About Picante</h2>
 <p>The Picante package provides tools for <strong>P</strong>hylocom <strong>i</strong>ntegration, <strong>c</strong>ommunity <strong>a</strong>nalyses, <strong>n</strong>ull-models, <strong>t</strong>raits and <strong>e</strong>volution in R.</p>
 <p>The package includes functions for analyzing the phylogenetic and trait diversity of ecological communities, comparative analyses, and the display and manipulation of phenotypic and phylogenetic data.</p>
-<p><strong>Package maintainer:</strong> Steven Kembel</p>
-<p><strong>Developers:</strong> Peter Cowan, Matthew Helmus, Steven Kembel</p>
-<p><strong>Contributors:</strong> David Ackerly, Simon Blomberg, Will Cornwell, Peter Cowan, Matthew Helmus, Steven Kembel, Helene Morlon, Cam Webb<p>    
-<p>Development of picante has been supported by <a href="http://nserc.ca">NSERC</a>, <a href="http://www.nescent.org/index.php">NESCent</a>, the <a href="http://code.google.com/soc/2008/">Google Summer of Code</a>, and the <a href="http://www.moore.org/">Gordon and Betty Moore Foundation</a>.</p>
-<p>Thanks to Jonathan Davies, Kyle Dexter, Catherine Graham, Nathaniel Hallinan, Nick Matzke, Alain Paquette, Emmanuel Paradis, Juan Parra, Dan Rabosky, and Marten Winter for feedback and bug reports. Thanks to <a href="http://r-forge.r-project.org">R-Forge</a> for hosting the project.</p>
 
 <h2>News</h2>
-<ul>
-<li>Picante 1.6 has been released
-<ul>
-<li>Added methods for phylogenetic estimation of trait values for unobserved taxa (Kembel et al. 2012 PLOS Comp Biol).</li>
-</ul>
-</li>
-</ul>
 
-<h2>Current features</h2>
-<ul>
-<li>Community phylogenetic and trait diversity</li>
-    <ul>
-    <li>Faith's PD (phylogenetic diversity), standardized effect size of PD, expected PD of subsets of a phylogenetic tree.</li>
-    <li>Rapid estimation of PD (O'Dwyer et al. 2012)</li>
-    <li>Webb's NRI/NTI and related measures of standardized effect size of community phylogenetic structure.</li>
-    <li>Mean pairwise distance and mean distance to nearest taxon among co-occurring species (can be used with any interspecific distance measure). Distances can be calculated based on presence or abundance in samples.</li>
-    <li>Rao's quadratic entropy, a measure of diversity within and among communities optionally taking phylogenetic distinctiveness into account.</li>
-    <li>Correlations and quantile regressions between species co-occurrence and phylogenetic distances</li>
-    <li>Phylogenetic species richness, evenness and variance of Helmus et al. (2007).</li>
-    <li>Phylogenetic community-environment regressions of Helmus et al. (2007).</li>
-    <li>Taxonomic and evolutionary distinctiveness of taxa for conservation biology.</li>
-    <li>Numerous phylogenetic beta diversity measures (PCD, phylosor, UniFrac, betaMPD, betaMNTD, Rao's quadratic entropy).</li>
-    </ul>
-<li>Phylogenetic signal (Blomberg <em>et al.</em>'s K statistic and P-value based on randomization test)</li>
-<li>Estimtion of trait values for unobserved taxa (Kembel et al. 2012 PLOS Comp Biol)</li>
-<li>Null models for community and phylogeny randomization</li>
-    <ul>
-    <li>Shuffle taxa labels across tips of phylogeny</li>
-    <li>Randomize community co-occurrence data
-        <ul>
-        <li>maintaining species occurrence frequency</li>
-        <li>maintaining sample species richness</li>
-        <li>maintaining both species occurrence frequency and sample species richness using independent swap (Gotelli 2000) or trial swap (Miklos and Podani 2004)</li>
-        </ul>
-    </li>
-    </ul>
-<li>Ives and Godfray's (2006) phylogenetic bipartite linear model.</li>
-<li>Ives and Helmus's (2011) phylogenetic generalized linear mixed models.</li>
-<li>Utility functions to read/write data in <a href="http://phylodiversity.net/phylocom/">Phylocom</a> format</li>
-<li>Tree plotting and labelling functions</li>
-</ul>
+<h3>Development of picante has been moved to Github. Please update your booksmarks to the new homepage for picante at <a href="https://github.com/skembel/picante">https://github.com/skembel/picante</a>.</h3>
 
-<h2>Obtaining picante</h2>
-<ul>
-<li>Version 1.6-0 is available on <a href="http://cran.r-project.org/">CRAN</a>. Simply type <strong><code>install.packages("picante")</code></strong> from within R.</li>
-<li>You can grab the latest nightly build <a href="http://r-forge.r-project.org/R/?group_id=134">here</a>, or by typing <strong><code>install.packages("picante",repos="http://R-Forge.R-project.org")</code></strong> from within R.</li>
-</ul>
+<p><strong>The version of the code at R-Forge is out of date and no longer updated.</strong> For bug reports, feature requests, and discussion please use Github.</h4>
 
-<h2>Obtaining help and more information about picante</h2>
-<p>There is a package vignette with more information about Picante data formats and analysis examples. Download it <a href="picante-intro.pdf">here</a>, or type <code><strong>vignette("picante-intro")</strong></code> to view within R.
-<p>Visit the <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>project summary page</strong></a> for bug reporting, feature requests, discussions, mailing lists, and access to the source code. If you have problems or questions about the code, please read the function documentation ( <code><strong>help(picante)</strong></code> ), and get in touch with us using one of the above methods.</p>
-
-<h2>Citing picante</h2>
-<p>If you use our software, please acknowledge our work by citing us:
-<ul><li>
-S.W. Kembel, P.D. Cowan, M.R. Helmus, W.K. Cornwell, H. Morlon, D.D. Ackerly, S.P. Blomberg, and C.O. Webb. 2010. Picante: R tools for integrating phylogenies and ecology. Bioinformatics 26:1463-1464.
-</li></ul>
-</p>
-
-<h2>Release history</h2>
-<p><ul>
-<li>Version 1.6: Add phyEstimate and phyEstimateDisc - phylogenetic trait estimation for unobserved taxa (Kembel et al. 2012).</li>
-<li>Version 1.5: Add expected.pd and ead functions to estimate expected phylogenetic diversity and edge abundance distribution (O'Dwyer et al. 2012).</li>
-<li>Version 1.4: Add pglmm methods from Ives & Helmus (2011). Correct typos in vignette and documentation. Remove pic.circular function until further testing can be performed.</li>
-<li>Version 1.3: Add function pcd. Fix single-community error in pse.</li>
-<li>Version 1.2: Bugfix release. Fix to evol.distinct function. Kcalc now behaves better with non-ultrametric trees.</li>
-<li>Version 1.1-1: Minor update to address deprecation of evolve.phylo function in ape package; deleted evolve.brownian function and changed example code.</li>
-<li>Version 1.1: Added package vignette, new example data set from Ives & Godfray (2006), function example code. Reinstated "richness" and "frequency" null models for ses.* functions.</li>
-<li>Version 1.0: Added raoD, unifrac, taxonomic and evolutionary distinctiveness functions. Modified phylosor to work with non-ultrametric phylogenies. Changed name of randomizeSample to randomizeMatrix. Added functions for checking taxa label matching between phylogenies and other data.</li>  
-<li>Version 0.7-1: Added ses.pd function</li>
-<li>Version 0.7: comm.phylo.qr function (quantile regression of co-occurrence and phylogenetic distance). traitgram function. Changed mnnd terminology to mntd. Added phylogenetic beta diversity measures comdist/comdistnt. Abundance weighting of mpd/mntd.
-<li>Version 0.6: Bugfix release. Changes to pd and phylosor functions.</li>
-<li>Version 0.5: Phylogenetic beta diversity measures (phylosor) with randomization testing.</li>
-<li>Version 0.4: New features and greatly improved speed of community randomizations (randomizeSample). New phylogenetic bipartite linear models functionality added.</li>
-<li>Version 0.3: Bugfixes. Added phylogenetic bipartite linear models, phylogenetic community-environment regressions.</li>
-<li>Version 0.2: Added new community phylogenetic structure metrics (PSR/PSV/PSC methods of Helmus et al.) and phylogenetic species richness accumulation curves</li>
-<li>Version 0.1: Initial public release</li>
-</ul>
-</p>
 
 <h2>Picante: the movie</h2>
 <p>If you liked the software, you'll love this movie depicting the history of Picante development from version 0.0 to 1.0!</p>
